c6b3bb9326f62325426245df26a473a4c03bc1d8
Diego Lopes Lima <contact.diego.lima@gmail.com>
2013-02-01 15:33:03 -0200

    Removed the quotes of selectors

--- jquery-validate.js
+++ jquery-validate.js
@@ -3,7 +3,7 @@
 
 	var
 
-		type = ['input:not([type]),input[type="color"],input[type="date"],input[type="datetime"],input[type="datetime-local"],input[type="email"],input[type="file"],input[type="hidden"],input[type="month"],input[type="number"],input[type="password"],input[type="range"],input[type="search"],input[type="tel"],input[type="text"],input[type="time"],input[type="url"],input[type="week"],textarea', 'select', 'input[type="checkbox"],input[type="radio"]'],
+		type = ['input:not([type]),input[type=color],input[type=date],input[type=datetime],input[type=datetime-local],input[type=email],input[type=file],input[type=hidden],input[type=month],input[type=number],input[type=password],input[type=range],input[type=search],input[type=tel],input[type=text],input[type=time],input[type=url],input[type=week],textarea', 'select', 'input[type=checkbox],input[type=radio]'],
 
 		// All field types
 		allTypes = type.join(','),
