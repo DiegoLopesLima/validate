14503f4909b332c1fba2743369b6a95a5839fb21
DiegoLopesLima <contact.diego.lima@gmail.com>
2013-12-19 17:54:15 -0200

    Initialization of custom selectors

--- validate.js
+++ validate.js
@@ -563,38 +563,6 @@
 				return value !== undefined ? $(this).data(name, data) : response;
 
 			}
-		},
-
-		selector = {
-			valid : function() {
-
-				return false;
-
-			},
-
-			invalid : function(filter) {
-
-				return false;
-
-			},
-
-			filled : function() {
-
-				return false;
-
-			},
-
-			empty : function() {
-
-				return false;
-
-			},
-
-			validated : function() {
-
-				return false;
-
-			}
 		};
 
 	$.fn[name] = function(options) {
@@ -622,21 +590,16 @@
 			return extend(plugin, index, value);
 
 		},
-		selector : function(index, value) {
-
-			return extend(selector, index, value);
-
-		},
 		version : '2.0.0'
 	});
 
 	$.expr[':'][name] = function(node, index, params) {
 
-		var
+		// node
 
-			param = (params[3] || '').split(':');
+		// params[3]
 
-		return !!selector[param[0]].call(node, $.trim(param[1]));
+		return false;
 
 	};
 
